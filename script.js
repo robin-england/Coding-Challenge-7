b0dd0d27cbedcca53a84d4ef14543517fc9ac7e6
robin-england <robin.england94@googlemail.com>
2024-06-02 16:07:11 -0400

    Implement form validation and feedback display logic.

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {  // This prevents the script from running before the DOM has loaded, preventing erros
 
+    document.getElementById("feedbackForm").addEventListener("submit", (event)=>{
+        event.preventDefault()  // Stops default form submission
 
-document.getElementById("feedbackForm").addEventListener("submit", (event)=>{
-    event.preventDefault()
+        const firstName = document.getElementById("firstName").value 
+        const lastName = document.getElementById("lastName").value
+        const rating = document.getElementById("rating").value
+        const comments = document.getElementById("comments").value  // Creates variables to receive inputs from form submission
 
-    const firstName = document.getElementById("firstName").value
-    const lastName = document.getElementById("lastName").value
-    const rating = document.getElementById("rating").value
-    const comments = document.getElementById("comments").value
-
-    document.getElementById("feedbackSubmission").innerHTML = `<h3>Feedback Submitted!</h3>
-    <b>First Name:</b> ${firstName} <br>
-    <b>Last Name:</b> ${lastName} <br>
-    <b>Rating (1-5):</b> ${rating} <br>
-    <b>Comments:</b><br>${comments}
-    `
-})
+        document.getElementById("feedbackSubmission").innerHTML = `<h3>Feedback Submitted!</h3> 
+            <b>First Name:</b> ${firstName} <br>
+            <b>Last Name:</b> ${lastName} <br>
+            <b>Rating (1-5):</b> ${rating} <br>
+            <b>Comments:</b><br>${comments}
+            ` // Displays inputs in HTML 
+        document.getElementById("feedbackForm").reset()}) // Clears values in the form so that new values can be entered easily
 })
